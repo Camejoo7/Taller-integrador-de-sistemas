ebbbfd1a7a86dfc777048d6ec399fd7c0ef81831
Camejoo7 <150731353+Camejoo7@users.noreply.github.com>
2025-08-29 18:38:56 -0300

    Delete Taller1 directory

--- Taller1/src/Logica/Administrador.java
+++
@@ -1,6 +0,0 @@
-package Logica;
-public class Administrador extends Usuario {
-    public Administrador(int ci, String nombre, String apellido, String contraseña) {
-        super(ci, nombre, apellido, contraseña);
-    }
-}
